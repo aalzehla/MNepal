1a8ea8df0678f0fd82a08665a48d7c2da428b0ab
unknown <phajumanoj@gmail.com>
2020-04-12 19:05:53 +0545

    Notification API Updated ie Use of Basic Authentication

---
+++ MNepalAPI/MNepalAPI/MNepalAPI/MNepalAPI/BasicAuthentication/ApiSecurity.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace MNepalAPI.BasicAuthentication
+{
+    public class ApiSecurity
+    {
+        public string AuthenticateAndLoad(string uname, string password)
+        {
+            string strUName = ConfigurationManager.AppSettings["UserName"];
+            string strPassword = ConfigurationManager.AppSettings["Password"];
+
+            if (uname == strUName && password == strPassword)
+            {
+                return "authenticate";
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- MNepalWeb/MNSuperadmin/Controllers/NotificationsController.cs
+++ MNepalWeb/MNSuperadmin/Controllers/NotificationsController.cs
@@ -7,6 +7,9 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using static MNSuperadmin.Models.Notifications;
+using System.Configuration;
+using System.Net;
+using System.Net.Http.Headers;
 
 namespace MNSuperadmin.Controllers
 {
@@ -32,15 +35,15 @@ namespace MNSuperadmin.Controllers
                 ViewBag.SenderMobileNo = userName;
 
 
-               
 
-                    if (this.TempData["registration_message"] != null)
-                    {
-                        this.ViewData["registration_message"] = this.TempData["registration_message"];
-                        this.ViewData["message_class"] = this.TempData["message_class"];
-                    }
 
-                    int id = TraceIdGenerator.GetID() + 1;
+                if (this.TempData["registration_message"] != null)
+                {
+                    this.ViewData["registration_message"] = this.TempData["registration_message"];
+                    this.ViewData["message_class"] = this.TempData["message_class"];
+                }
+
+                int id = TraceIdGenerator.GetID() + 1;
                 string stringid = (id).ToString();//this.GetID() + 1
                 string traceID = stringid.PadLeft(11, '0') + 'W';
                 ViewBag.TraceID = traceID;
@@ -67,8 +70,6 @@ namespace MNSuperadmin.Controllers
 
             TempData["userType"] = userType;
 
-
-
             this.ViewData["userType"] = this.TempData["userType"];
             ViewBag.UserType = this.TempData["userType"];
             ViewBag.Name = name;
@@ -79,8 +80,6 @@ namespace MNSuperadmin.Controllers
             string mobile = userName; //mobile is username
             TraceIdGenerator _tig = new TraceIdGenerator();
             var tid = _tig.GenerateTraceID();
-            
-
 
             var payload = new Notificationsobject
             {
@@ -98,7 +97,7 @@ namespace MNSuperadmin.Controllers
 
             int responseCode = 0;
             string message = string.Empty;
-            string responsetext = string.Empty;
+            string responsetext = "Unauthorized";
             bool result = false;
 
             // Serialize our concrete class into a JSON String
@@ -107,26 +106,24 @@ namespace MNSuperadmin.Controllers
             // Wrap our JSON inside a StringContent which then can be used by the HttpClient class
             var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
             var PushNotificationUrl = System.Configuration.ConfigurationManager.AppSettings["PushNotificationUrl"];
-
             using (var httpClient = new HttpClient())
             {
+                var BasicAuthUserName = ConfigurationManager.AppSettings["BasicAuthUserName"];
+                var BasicAuthPassword = ConfigurationManager.AppSettings["BasicAuthPassword"];
+                var byteArray = new UTF8Encoding().GetBytes(BasicAuthUserName + ":" + BasicAuthPassword);
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
                 var httpResponse = await httpClient.PostAsync(PushNotificationUrl, httpContent);
-
-                try
+                if (httpResponse.IsSuccessStatusCode)
                 {
-                    if (httpResponse.IsSuccessStatusCode)
+                    try
                     {
-
                         result = true;
-                        responseCode = (int)_res.StatusCode;
+                        responseCode = (int)httpResponse.StatusCode;
                         responsetext = await httpResponse.Content.ReadAsStringAsync();
                         message = httpResponse.Content.ReadAsStringAsync().Result;
                         string respmsg = "";
                         if (!string.IsNullOrEmpty(message))
                         {
-
-
-
                             int code = 200;
                             respmsg = "Success";
                             if (code != responseCode)
@@ -136,29 +133,31 @@ namespace MNSuperadmin.Controllers
                         }
                         return Json(new { responseCode = responseCode, responseText = respmsg },
                         JsonRequestBehavior.AllowGet);
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        return Json(new { responseCode = "400", responseText = ex.Message },
+                            JsonRequestBehavior.AllowGet);
+                    }
+                }
+               
+                 else
+                {
+                    result = false;
+                    responseCode = (int)httpResponse.StatusCode;
+                    if (responsetext == null)
+                    {
+                        return Json(new { responseCode = responseCode, responseText = responsetext },
+                    JsonRequestBehavior.AllowGet);
                     }
                     else
                     {
-                        result = false;
-                        responseCode = (int)_res.StatusCode;
-                        responsetext = await _res.Content.ReadAsStringAsync();
-                        if (message == null)
-                        {
-                            return Json(new { responseCode = responseCode, responseText = responsetext },
-                        JsonRequestBehavior.AllowGet);
-                        }
-                        else
-                        {
 
-                            return Json(new { responseCode = responseCode, responseText = responsetext },
-                            JsonRequestBehavior.AllowGet);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    return Json(new { responseCode = "400", responseText = ex.Message },
+                        return Json(new { responseCode = responseCode, responseText = responsetext },
                         JsonRequestBehavior.AllowGet);
+                    }
                 }
             }
 
