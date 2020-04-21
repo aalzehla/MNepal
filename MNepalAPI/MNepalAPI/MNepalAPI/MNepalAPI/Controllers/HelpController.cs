92d16d399e30b61c45356f2c379655ce05dc01e0
unknown <phajumanoj@gmail.com>
2020-04-21 17:59:01 +0545

    addition of help api

---
+++ MNepalAPI/MNepalAPI/MNepalAPI/MNepalAPI/Controllers/HelpController.cs
@@ -0,0 +1,35 @@
+﻿using MNepalAPI.BasicAuthentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MNepalAPI.Controllers
+{
+    [MyBasicAuthenticationFilter]
+    public class HelpController : ApiController
+    {
+        [HttpGet]
+        public async Task<HttpResponseMessage> ContactUs()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                // Do the actual request and await the response
+                var URL = System.Configuration.ConfigurationManager.AppSettings["HelpUrl"];
+                var httpResponse = await httpClient.GetAsync(URL);
+
+                // If the response contains content we want to read it!
+                if (httpResponse.Content != null)
+                {
+                    var responseContent = await httpResponse.Content.ReadAsStringAsync();
+                    return Request.CreateResponse(HttpStatusCode.OK, responseContent);
+                }
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ MNepalAPI/MNepalAPI/MNepalAPI/MNepalAPI/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MNepalAPI.WebApiApplication" Language="C#" %>
