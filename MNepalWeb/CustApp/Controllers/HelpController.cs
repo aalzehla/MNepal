1b589125d9acb8fadc0345cc60c9e114b7e9bf34
unknown <phajumanoj@gmail.com>
2020-08-30 13:19:02 +0545

    changes made to privacy policy, contactus, terms and conditions, aboutus

--- MNepalWeb/CustApp/Controllers/HelpController.cs
+++ MNepalWeb/CustApp/Controllers/HelpController.cs
@@ -9,33 +9,33 @@ namespace CustApp.Controllers
 {
     public class HelpController : Controller
     {
-        public readonly Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        //public readonly Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         // GET: Help
         public ActionResult ContactUs()
         {
-            try
-            {
-                string clientIp = (Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ??
-                   Request.ServerVariables["REMOTE_ADDR"]).Split(',')[0].Trim();
-                var eventInfo = new LogEventInfo(LogLevel.Info, Logger.Name, "Message");
-                eventInfo.Properties["UserName"] = "";
-                eventInfo.Properties["UserIP"] = clientIp;
-                Logger.Log(eventInfo);
-            }
-            catch (Exception ex)
-            {
+            //try
+            //{
+            //    string clientIp = (Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ??
+            //       Request.ServerVariables["REMOTE_ADDR"]).Split(',')[0].Trim();
+            //    var eventInfo = new LogEventInfo(LogLevel.Info, Logger.Name, "Message");
+            //    eventInfo.Properties["UserName"] = "";
+            //    eventInfo.Properties["UserIP"] = clientIp;
+            //    Logger.Log(eventInfo);
+            //}
+            //catch (Exception ex)
+            //{
 
-                var eventInfo = new LogEventInfo(LogLevel.Error, Logger.Name, ex.ToString());
-                eventInfo.Properties["UserName"] = "";
-                eventInfo.Properties["Exception"] = ex.Message;
-                Logger.Log(eventInfo);
-            }
+            //    var eventInfo = new LogEventInfo(LogLevel.Error, Logger.Name, ex.ToString());
+            //    eventInfo.Properties["UserName"] = "";
+            //    eventInfo.Properties["Exception"] = ex.Message;
+            //    Logger.Log(eventInfo);
+            //}
             return View();
         }
 
         public ActionResult AboutUs()
         {
-            return Redirect("http://mnepal.com/");
+            return Redirect("https://nibl.com.np/");
         }
 
         public ActionResult Career()
