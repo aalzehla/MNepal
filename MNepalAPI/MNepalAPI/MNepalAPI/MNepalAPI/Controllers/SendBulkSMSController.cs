719a2414f987c3bd945a24f7d69eeae0a82841bc
unknown <phajumanoj@gmail.com>
2020-09-15 17:56:44 +0545

    addition of sendbulksms api

---
+++ MNepalAPI/MNepalAPI/MNepalAPI/MNepalAPI/Controllers/SendBulkSMSController.cs
@@ -0,0 +1,61 @@
+﻿using MNepalAPI.BasicAuthentication;
+using MNepalAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MNepalAPI.Controllers
+{
+    [MyBasicAuthenticationFilter]
+    public class SendBulkSMSController : ApiController
+    {
+        /// <summary>
+        /// use ? for newline in textMessage and seperate mobileNumber with comma(,) for multiple
+        /// </summary>
+
+        [HttpPost]
+        public async Task<List<Category>> BulkSMS(string mobileNumber, string textMessage)
+        {
+            string SMSNTC = ConfigurationManager.AppSettings["MNepalNTCSMSServerUrl"];
+            string SMSNCELL = System.Web.Configuration.WebConfigurationManager.AppSettings["MNepalSMSServerUrl"];
+
+            string messageToSend = "";
+            //for newline replace begins 
+            string input = textMessage;
+            string pattern = "[?]";
+            string replacement = Environment.NewLine;
+            Regex rgx = new Regex(pattern);
+            string result = rgx.Replace(input, replacement);
+            //newline replace ends
+
+            messageToSend += result;
+
+            var client = new HttpClient();
+            List<Category> categoryRepo = new List<Category>();
+            String[] mobileNumberRepo = mobileNumber.Split(',');  // seperate mobile number with ,
+
+            foreach (var mobNumber in mobileNumberRepo)
+            {
+                if ((mobNumber.Substring(0, 3) == "980") || (mobNumber.Substring(0, 3) == "981")) //FOR NCELL
+                {
+                    var content = client.GetAsync(
+                        SMSNCELL + "977" + mobNumber + "&Text=" + messageToSend + "");
+                }
+                else if ((mobNumber.Substring(0, 3) == "985") || (mobNumber.Substring(0, 3) == "984")
+                            || (mobNumber.Substring(0, 3) == "986"))
+                {
+                    var content = client.GetAsync(
+                        SMSNTC + "977" + mobNumber + "&Text=" + messageToSend + "");
+                }
+
+            }
+            return categoryRepo;
+        }
+    }
+}
---
+++ MNepalAPI/MNepalAPI/MNepalAPI/MNepalAPI/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MNepalAPI.Models
+{
+    public class Category
+    {
+        public String mobileNumber { get; set; }
+        public String message { get; set; }
+    }
+}
